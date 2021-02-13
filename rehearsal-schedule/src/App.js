df221efddb0adf277853be285f0be066ae7010c8
Craig Turnbull <cturnbull509@gmail.com>
2021-02-13 10:36:02 -0800

    Added header

--- rehearsal-schedule/src/App.js
+++ rehearsal-schedule/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import logo from './wildcat.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="CWU" />
+        <h1 className="App-Title">
+          CWU Rehearsal Calendar
+        </h1>
+      </header>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      </p>
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+      </a>
     </div>
   );
 }
