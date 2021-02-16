26f1c621781e71b47e205ea90207c1ad24a1bc12
Kirsten B <kboyles138@gmail.com>
2021-02-16 10:30:54 -0800

    file source

--- rehearsal-schedule/src/App.js
+++ rehearsal-schedule/src/App.js
@@ -11,16 +11,9 @@ function App() {
         </h1>
       </header>
       <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
       </p>
-      <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-      </a>
+      
     </div>
   );
 }
